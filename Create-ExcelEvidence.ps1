c265cc9cb95ac42091fa186a570cbf8315eaec79
asterisk9101 <tm11b028@gmail.com>
2016-10-02 18:11:16 +0900

    次のセルへの移動を簡略化することで高速化した

--- Create-ExcelEvidence.ps1
+++ Create-ExcelEvidence.ps1
@@ -32,15 +32,9 @@ while($true){
         $Excel.ActiveCell.Offset(1, 0).Select() > $null
         $Excel.ActiveSheet.Paste()
         $Graphic = $Excel.Selection
-        $Offset = $Excel.ActiveCell.Top + $Graphic.Height
         measure-command {
             $Excel.Caption = "Pasting..."
-            $cell = $Excel.ActiveCell
-            $cell = $cell.Offset([Math]::Ceiling($Graphic.Height / $cell.Height), 0)
-            while ($cell.Top -lt $Offset) { $cell = $cell.Offset( 5,0)}
-            while ($cell.Top -gt $Offset) { $cell = $cell.Offset(-5,0)}
-            while ($cell.Top -lt $Offset) { $cell = $cell.Offset( 1,0)}
-            while ($cell.Top -gt $Offset) { $cell = $cell.Offset(-1,0)}
+            $cell = $Excel.ActiveSheet.Cells.Item($Graphic.BottomRightCell.Row, $Graphic.TopLeftCell.Column)
             $cell.Offset(2,0).Select() > $null
             $Excel.Caption = $null
         } | % { logger $_.TotalMilliseconds.toString("#####.000ms").PadLeft(10) }
